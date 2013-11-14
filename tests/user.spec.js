8337337b54df74652f9f866c03dab794d600b8aa
kisPocok <david.schneidhoffer@gmail.com>
2013-11-14 17:38:53 +0100

    Introduce tests for User class

---
+++ tests/user.spec.js
@@ -0,0 +1,32 @@
+var User = require('../src/user').User,
+	Room = require('../src/room').room;
+
+describe("User", function()
+{
+	beforeEach(function()
+	{
+		roomName = 'default-room';
+		socketId = 'default-socket-id';
+		user = new User(socketId);
+		room = new Room(roomName);
+	});
+
+	it("default values", function()
+	{
+		expect(user.id).toBe(socketId);
+		expect(user.player).toBe(null);
+		expect(user.room).toBe(null);
+	});
+
+	it("update room with room class", function()
+	{
+		user.updateWithRoom(room);
+		expect(user.room).toEqual(room.name);
+	});
+
+	it("update room with string", function()
+	{
+		user.updateWithRoom(roomName);
+		expect(user.room).toEqual(roomName);
+	});
+});
